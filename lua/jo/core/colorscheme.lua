56d069e9a9ade3c71dd6e3d4d48bb55fa328dd09
Attia <jonas.attia@epita.fr>
2023-04-09 13:46:54 +0200

    update statusline and tabline

--- lua/jo/core/colorscheme.lua
+++ lua/jo/core/colorscheme.lua
@@ -1,40 +1,12 @@
--- set colorscheme to nightfly with protected call
--- in case it isn't installed
-local status, _ = pcall(vim.cmd, "colorscheme tokyonight-moon")
-if not status then
+-- set colorscheme to tokyonight with protected call
+local tokyo_status, tokyonight = pcall(require, "tokyonight")
+if not tokyo_status then
 	print("Colorscheme not found!") -- print error if colorscheme not installed
 	return
 end
 
-require("tokyonight").setup({
-	on_highlights = function(hl, c)
-		local prompt = "#2d3149"
-		hl.TelescopeNormal = {
-			bg = c.bg_dark,
-			fg = c.fg_dark,
-		}
-		hl.TelescopeBorder = {
-			bg = c.bg_dark,
-			fg = c.bg_dark,
-		}
-		hl.TelescopePromptNormal = {
-			bg = prompt,
-		}
-		hl.TelescopePromptBorder = {
-			bg = prompt,
-			fg = prompt,
-		}
-		hl.TelescopePromptTitle = {
-			bg = prompt,
-			fg = prompt,
-		}
-		hl.TelescopePreviewTitle = {
-			bg = c.bg_dark,
-			fg = c.bg_dark,
-		}
-		hl.TelescopeResultsTitle = {
-			bg = c.bg_dark,
-			fg = c.bg_dark,
-		}
-	end,
+tokyonight.setup({
+	style = "moon", -- 'storm', 'night', 'moon' and 'day'
 })
+
+vim.cmd([[colorscheme tokyonight]])
--- lua/jo/plugins/lualine.lua
+++ lua/jo/plugins/lualine.lua
@@ -4,53 +4,19 @@ if not status then
 	return
 end
 
--- get lualine nightfly theme
-local lualine_horizon = require("lualine.themes.horizon")
-
--- new colors for theme
-local new_colors = {
-	blue = "#65D1FF",
-	green = "#3EFFDC",
-	violet = "#FF61EF",
-	yellow = "#FFDA7B",
-	black = "#000000",
-	cyan = "#FFF000",
-}
-
--- change horizon theme colors
-lualine_horizon.command = {
-	a = {
-		gui = "bold",
-		bg = new_colors.cyan,
-		fg = new_colors.black, -- black
-	},
-}
-lualine_horizon.insert = {
-	a = {
-		gui = "bold",
-		bg = new_colors.blue,
-		fg = new_colors.black, -- black
-	},
-}
-
-lualine_horizon.visual = {
-	a = {
-		gui = "bold",
-		bg = new_colors.green,
-		fg = new_colors.black, -- black
-	},
-}
-
 -- configure lualine with modified theme
 lualine.setup({
 	options = {
 		icons_enabled = true,
-		theme = "auto",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
+		theme = "tokyonight",
+		-- component_separators = { left = "", right = "" },
+		-- section_separators = { left = "", right = "" },
+		component_separators = { left = " ", right = " " },
+		section_separators = { left = " ", right = " " },
+
 		disabled_filetypes = {
-			statusline = {},
-			winbar = {},
+			statusline = { "dashboard", "NvimTree" },
+			winbar = { "dashboard", "NvimTree" },
 		},
 		ignore_focus = {},
 		always_divide_middle = true,
@@ -60,14 +26,5 @@ lualine.setup({
 			tabline = 1000,
 			winbar = 1000,
 		},
-		--theme = lualine_horizon,
 	},
-	--tabline = {
-	--	lualine_a = { "buffers" },
-	--	lualine_b = {},
-	--	lualine_c = {},
-	--	lualine_x = {},
-	--	lualine_y = {},
-	--	lualine_z = {},
-	--},
 })
