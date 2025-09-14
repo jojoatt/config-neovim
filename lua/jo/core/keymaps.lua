d2c35a2f649a1cc5a97e5189fe5442470404815a
Jonas Attia <jonas.attia@epita.fr>
2025-09-14 22:24:44 +0200

    feat(treesitter): add treesitter plugin

--- lua/jo/core/keymaps.lua
+++ lua/jo/core/keymaps.lua
@@ -1,4 +1,3 @@
---local wk = require("which-key")
 -- set leader key to space
 vim.g.mapleader = " "
 
@@ -33,55 +32,3 @@ keymap.set("n", "<leader>er", ":NvimTreeRefresh<CR>", { desc = "Refresh file exp
 keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Fuzzy find files in current working directory" })
 keymap.set("n", "<leader>fr", ":Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
 keymap.set("n", "<leader>fs", ":Telescope live_grep<CR>", { desc = "Find string in current working directory" })
-
---toggle-term
-keymap.set("t", "<esc>", [[<C-\><C-n>]], opts) --go from Terminal mode to normal mode
-keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
-keymap.set("t", "<leader>c", [[<C-\><C-n>:q!<CR>]], opts)
-keymap.set("n", "<leader>tf", ":ToggleTerm direction=float<CR>") --open new floating terminal
-
---wk.register({
-	--t = { ":ToggleTerm<CR>", "Open new terminal" },
---}, { prefix = "<leader>" })
-
--- nvim-tree
---wk.register({
-	--e = { ":NvimTreeToggle<CR>", "FileExplorer" },
---}, { prefix = "<leader>" })
-
---See all diagnostics
---wk.register({
-	--d = { ":Trouble<CR>", "See diagnostics" },
---}, { prefix = "<leader>" })
-
---Tabline Keymaps
---wk.register({
-	--n = { ":TablineBufferNext<CR>", "NextBuffer" },
-	--p = { ":TablineBufferPrevious<CR>", "PrevBuffer" },
---}, { prefix = "<leader>" })
-
---Close buffer
---wk.register({
-	--c = { ":bd<CR>", "CloseBuffer" },
---}, { prefix = "<leader>" })
-
--- telescope
---wk.register({
-	--f = { ":Telescope find_files<CR>", "Find File" }, -- find files within current working directory, respects .gitignore
-	--s = { ":Telescope live_grep<CR>", "Find String" }, -- find string in current working directory as you type
-	--b = { ":Telescope buffers<CR>", "List Open Buffers" }, -- list open buffers in current neovim instance
-	--h = { ":Telescope help_tags<CR>", "Help Tags" }, -- list available help tags
---}, { prefix = "<leader>f" })
-
---list of colorscheme via telescope
---wk.register({
-	--s = { ":Telescope colorscheme<CR>", "Colorscheme List" },
---}, { prefix = "<leader>c" })
-
---wk.register({
---	D = { ":lua vim.lsp.buf.declaration()<CR>", "Go To Declaration", opts }, -- got to declaration
---	d = { ":lua vim.lsp.buf.definition()<CR>", "Go To Definition", opts }, -- got to definition
---}, { prefix = "g" })
---
+++ lua/jo/plugins/treesiter.lua
@@ -0,0 +1,62 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
+  lazy = false,
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+  },
+  config = function()
+    local treesitter = require("nvim-treesitter.configs")
+
+    -- configure treesitter
+    treesitter.setup({
+      -- enable syntax highlighting
+      highlight = {
+        enable = true,
+      },
+      -- enable indentation
+      indent = {
+        enable = true,
+      },
+      -- enable autotagging
+      autotag = {
+        enable = true,
+      },
+      -- ensure these language parsers are installed
+      ensure_installed = {
+        "angular",
+        "bash",
+		    "c",
+		    "cpp",
+        "cmake",
+        "c_sharp",
+		    "css",
+		    "dockerfile",
+		    "gitignore",
+		    "graphql",
+		    "html",
+        "hcl",
+        "helm",
+        "terraform",
+	  	  "javascript",
+        "python",
+		    "json",
+		    "lua",
+		    "java",
+        "go",
+        "xml",
+		    "markdown",
+		    "prisma",
+		    "regex",
+		    "rust",
+		    "scss",
+		    "toml",
+		    "tsx",
+		    "typescript",
+		    "vim",
+      },
+    })
+  end
+}
